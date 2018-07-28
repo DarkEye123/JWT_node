f0b5ea2cdd7f46da6d03906e7221fa044701401f
DarkEye <lesko.matej.pu@gmail.com>
2018-07-28 13:24:08 +0200

    verification of user (middleware) refactored

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,6 +1,19 @@
-export function verifyUserBody(resp) {
-    if (resp.body !== undefined && resp.body.username !== "" && resp.body.password !== "") {
-        return;
+export function verifyUserBody(req) {
+    let properties = [];
+    if (req.body === undefined) {
+        throw new Error("username and password are missing");
     }
-    return new Error("Unauthorized");
+    if (!req.body.username || req.body.username === "") {
+        properties.push("username");
+    }
+    if (!req.body.password || req.body.password === "") {
+        properties.push("password");
+    }
+    if (properties.length > 0) {
+        if (properties.length > 1) {
+            let msg = properties.join(" and ");
+            throw new Error(msg + " are missing or are empty");
+        }
+        throw new Error(properties[0] + " is missing or is empty");
+    }
 }
