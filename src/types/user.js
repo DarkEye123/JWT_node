92b16ab0a73c3080c36bc1dac2a196e7a3e359cb
DarkEye <lesko.matej.pu@gmail.com>
2018-07-28 13:31:13 +0200

    user type reworked

--- src/types/user.js
+++ src/types/user.js
@@ -1,6 +1,23 @@
 export class User {
-    constructor(resp) {
-        this.username = resp.body.username;
-        this.password = resp.body.password;
+    constructor(id, username, password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
+
+function isResponseBodyValid(resp) {
+    if (resp.body && resp.body.username && resp.body.password) {
+        return true;
+    }
+    return false;
+}
+
+export function CreateUserFromResponse(resp) {
+    if (isResponseBodyValid(resp)) {
+        return new User(null, resp.body.username, resp.body.password);
+    }
+    else {
+        return null;
     }
 }
