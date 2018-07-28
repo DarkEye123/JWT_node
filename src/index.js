2be2db61d9578adaea69d5ce5d2d0bcb5010029c
DarkEye <lesko.matej.pu@gmail.com>
2018-07-28 15:20:38 +0200

    JWT used in message response

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import {verifyUserBody} from "./middlewares/user";
 import {AuthUser} from "./db/in_mem";
 const express = require("express");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+const SECRET = process.env.SECRET || "superkualalumpursecret";
 
 app.use(bodyParser.json());
 
@@ -27,9 +29,14 @@ app.post("/login", (req, res) => {
             .sendStatus(401);
         return;
     }
+    const token = jwt.sign({
+        sub: user.id,
+        username: user.username
+    }, SECRET, {expiresIn:"3 hours"});
+
     res
         .status(200)
-        .send(`You successfully logged in with ${user.username}`);
+        .send({message:`You successfully logged in with ${user.username}`, access_token:token});
 });
 
 app.get("/status", (_, resp) => {
